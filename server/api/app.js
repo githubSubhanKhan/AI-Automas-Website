c21ab7dfa25498833b96ae20ddfa1520462105bf
githubSubhanKhan <subbikhan124@gmail.com>
2026-02-12 21:55:26 +0500

    added test url

--- server/api/app.js
+++ server/api/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://aiautomas.com","https://aiautomas.vercel.app"],
+    origin: ["http://localhost:5173", "https://aiautomas.com","https://aiautomas.vercel.app", "https://ai-automas.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
