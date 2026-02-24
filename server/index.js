ee2d4266a59ae44e3e980dc97ff2f73d83760d2f
githubzohaib <zohaibaliwork@gmail.com>
2026-02-24 18:26:08 +0500

    Refactor server startup code for improved readability

--- server/index.js
+++ server/index.js
@@ -9,4 +9,11 @@ connectDB();
 
 app.get("/", (req, res) => res.send("Backend is running!"));
 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
 module.exports = app;
